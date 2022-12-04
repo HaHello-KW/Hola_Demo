20d7fe700cd88cd27fe3de7bcef3b52107031197
ymj307 <dymj307@gmail.com>
2022-12-05 01:04:40 +0900

    change

--- App.tsx
+++ App.tsx
@@ -4,8 +4,12 @@ import {Stack, Button} from '@react-native-material/core';
 const App = () => (
   <Stack fill center spacing={10}>
     <Button title="버튼구현 1" color="#F2F2F2" />
-    <Button title="버튼구현 2" color="#F2F2F2" />
-    <Button title="다음 버튼 구현" color="#F47100" />
+    <Button title=" 버튼구현 2" color="#F2F2F2" />
+    <Button
+      title="다음 버튼 구현"
+      color="#F47100"
+      onPress={() => alert('to the next')}
+    />
   </Stack>
 );
 
