fd208eb14d972813aba48fc68b511ef1004d5167
ymj307 <dymj307@gmail.com>
2022-12-20 15:15:49 +0900

    need to make style.tsx using stylesheets 스타일화 필요

--- App.tsx
+++ App.tsx
@@ -1,20 +1,104 @@
 import React from 'react';
-import {Stack, Button} from '@react-native-material/core';
+import {
+  AppBar,
+  Stack,
+  Button,
+  ActivityIndicator,
+  FAB,
+  IconButton,
+  Avatar,
+  Box,
+  HStack,
+} from '@react-native-material/core';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {textAlign} from '@mui/system';
 
 const App = () => (
-  <Stack fill center spacing={10}>
-    <Button title="버튼구현 1" color="#F2F2F2" />
-    <Button
-      title=" 버튼구현 2"
-      color="#F2F2F2"
-      onPress={() => alert('for testing')}
-    />
-    <Button
-      title="다음 버튼 구현"
-      color="#F47100"
-      onPress={() => alert('to the next')}
-    />
-  </Stack>
+  <>
+    <Box w="100%" h={165} style={{backgroundColor: '#FFAF49'}}>
+      <HStack style={styles.boxline} fill center spacing={8}>
+        <Box w="12%" h={2} style={styles.boxlineDone} />
+        <Box w="12%" h={2} style={styles.boxlineDone} />
+        <Box w="12%" h={2} style={styles.boxlineYet} />
+        <Box w="12%" h={2} style={styles.boxlineYet} />
+        <Box w="12%" h={2} style={styles.boxlineYet} />
+        <Box w="12%" h={2} style={styles.boxlineYet} />
+        <Box w="12%" h={2} style={styles.boxlineYet} />
+      </HStack>
+    </Box>
+    <View style={styles.container}>
+      <Stack fill center spacing={10}>
+        {/* <Avatar icon={props => <Icon name="account" {...props} />} size={70} /> */}
+        <Stack fill center spacing={20} style={{width: '100%', height: '100%'}}>
+          <Button
+            style={styles.buttonSelection}
+            title="버튼구현 1"
+            titleStyle={{color: 'black'}}
+            onPress={() => alert('select no.1')}
+          />
+          <Button
+            style={styles.buttonSelection}
+            title="버튼구현 2"
+            titleStyle={{color: 'black'}}
+            onPress={() => alert('select no.2')}
+          />
+          <Button
+            style={styles.buttonSelection}
+            title="버튼구현 3"
+            titleStyle={{color: 'black'}}
+            onPress={() => alert('select no.3')}
+          />
+          <Button
+            style={styles.buttonSelection}
+            title="버튼구현 4"
+            titleStyle={{color: 'black'}}
+            onPress={() => alert('select no.4')}
+          />
+
+          <Button
+            style={styles.buttonNext}
+            title="다음 버튼"
+            titleStyle={{color: 'white', fontWeight: 'bold'}}
+            onPress={() => alert('to the next')}
+          />
+        </Stack>
+      </Stack>
+    </View>
+  </>
 );
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boxline: {
+    marginTop: 80,
+    marginBottom: '10%',
+    marginRight: '3%',
+    marginLeft: '3%',
+  },
+  boxlineDone: {
+    backgroundColor: '#F47100',
+  },
+  boxlineYet: {
+    backgroundColor: '#FFFFFF',
+  },
+  buttonSelection: {
+    backgroundColor: '#F2F2F2',
+    width: 300,
+    height: 40,
+  },
+  buttonNext: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#F47100',
+    width: 300,
+    height: 40,
+  },
+};
 
 export default App;
---
+++ android/app/src/main/assets/@types/react-native-vector-icons/index.d.ts
@@ -0,0 +1,3 @@
+// @types/모듈명/index.d.ts
+declare module 'react-native-vector-icons';
+declare module 'react-native-vector-icons/MaterialCommunityIcons';
